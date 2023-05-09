2e1372a6e213d0de708c677bd58b6f30c8baf743
metal <metal.walter@gmail.com>
2023-05-08 21:44:32 -0300

    Adicionar arquivos de projeto.

---
+++ MiniAgenda/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace MiniAgenda
+{
+    partial class Anotações
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.dgvNotas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Título";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 81);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mensagem";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(117, 21);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(160, 30);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(117, 81);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(160, 165);
+            this.txtMensagem.TabIndex = 1;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnNovo.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNovo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnNovo.Location = new System.Drawing.Point(9, 291);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(119, 33);
+            this.btnNovo.TabIndex = 2;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = false;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSalvar.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSalvar.Location = new System.Drawing.Point(158, 291);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(119, 33);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLer
+            // 
+            this.btnLer.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLer.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLer.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLer.Location = new System.Drawing.Point(307, 291);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(119, 33);
+            this.btnLer.TabIndex = 2;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = false;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDeletar.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeletar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnDeletar.Location = new System.Drawing.Point(456, 291);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(119, 33);
+            this.btnDeletar.TabIndex = 2;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = false;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // dgvNotas
+            // 
+            this.dgvNotas.AllowUserToAddRows = false;
+            this.dgvNotas.AllowUserToDeleteRows = false;
+            this.dgvNotas.AllowUserToResizeColumns = false;
+            this.dgvNotas.AllowUserToResizeRows = false;
+            this.dgvNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNotas.Location = new System.Drawing.Point(307, 21);
+            this.dgvNotas.Name = "dgvNotas";
+            this.dgvNotas.ReadOnly = true;
+            this.dgvNotas.RowHeadersVisible = false;
+            this.dgvNotas.Size = new System.Drawing.Size(268, 225);
+            this.dgvNotas.TabIndex = 3;
+            this.dgvNotas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvNotas_CellContentClick);
+            // 
+            // Anotações
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.dgvNotas);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Goudy Old Style", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Anotações";
+            this.Text = "Anotações";
+            this.Load += new System.EventHandler(this.Anotações_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNotas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.DataGridView dgvNotas;
+    }
+}
+
---
+++ MiniAgenda/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniAgenda
+{
+    public partial class Anotações : Form
+    {
+        DataTable table;
+        public Anotações()
+        {
+            InitializeComponent();
+        }
+
+        private void Anotações_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Título", typeof(string));
+            table.Columns.Add("Mensagem", typeof (string));
+
+            dgvNotas.DataSource = table;
+            dgvNotas.Columns["Mensagem"].Visible = false;
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            txtTitulo.Clear();
+            txtMensagem.Clear();
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitulo.Text, txtMensagem.Text);
+            btnNovo.PerformClick();
+
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            int index = dgvNotas.CurrentCell.RowIndex;
+            if (index > -1)
+            {
+                txtTitulo.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMensagem.Text = table.Rows[index].ItemArray[1].ToString();
+
+            }
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            int index = dgvNotas.CurrentCell.RowIndex;
+            table.Rows[index].Delete();
+        }
+
+        private void dgvNotas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
